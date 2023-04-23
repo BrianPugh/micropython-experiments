b51379495b4fddbeecb9cbdf086872097bf1d39f
Brian Pugh <bnp117@gmail.com>
2023-04-22 17:00:35 -0700

    misc

--- experiments/call-overhead/README.md
+++ experiments/call-overhead/README.md
@@ -1,5 +1,29 @@
 # Question
+How much overhead is there for various function calls?
 
 # Output
 
+```
+Name                        Calls    Total (%)     Total (ms)   Average (ms)
+----------------------------------------------------------------------------
+function_function           10000         1.03         417.75       0.041775
+function_viper_function     10000         0.97        392.747      0.0392747
+method                      10000         0.84        342.812      0.0342812
+function_function_viper     10000         0.82        334.705      0.0334705
+function                    10000         0.81        330.515      0.0330515
+method_viper                10000         0.71         287.51       0.028751
+function_viper              10000         0.69        279.264      0.0279264
+no_function                     1         0.14         55.485         55.485
+no_function_viper               1         0.01          2.588          2.588
+```
+
 # Conclusion
+* Not using a vanilla function (`no_function`) is 6x faster than using a function (`function`).
+
+* A viper loop is 21.4x faster than a vanilla loop.
+
+* Invoking a vanilla function is slightly more costly than a viper function, whether from a vanilla or viper function.
+
+* Calling a vanilla function inside a viper function has similar overhead to calling a vanilla function from a vanilla function.
+
+* Invoking a method is ever so slightly slower than invoking a function, probably due to additional lookup overhead.
--- experiments/call-overhead/main.py
+++ experiments/call-overhead/main.py
@@ -1,9 +1,89 @@
+import micropython
 import uprofiler
+
+uprofiler.print_period = 0
+
+
+class MyClass:
+    @uprofiler.profile
+    def method(self, a):
+        return a + 1
+
+    @uprofiler.profile(name="method_viper")
+    @micropython.viper
+    def method_viper(self, a: int) -> int:
+        return a + 1
 
 
 @uprofiler.profile
+def function(a):
+    return a + 1
+
+
+def _function_inner(a):
+    return a + 1
+
+
+@uprofiler.profile
+def function_function(a):
+    return _function_inner(a)
+
+
+@micropython.viper
+def _function_inner_viper(a: int) -> int:
+    return a + 1
+
+
+@uprofiler.profile(name="function_function_viper")
+@micropython.viper
+def function_function_viper(a: int) -> int:
+    return int(_function_inner_viper(a))
+
+
+@uprofiler.profile(name="function_viper_function")
+@micropython.viper
+def function_viper_function(a: int) -> int:
+    return int(_function_inner(a))
+
+
+@uprofiler.profile(name="function_viper")
+@micropython.viper
+def function_viper(a: int) -> int:
+    return a + 1
+
+
+@uprofiler.profile
+def no_function(iters):
+    a = 0
+    for i in range(iters):
+        a = i + 1
+    return a
+
+
+@uprofiler.profile(name="no_function_viper")
+@micropython.viper
+def no_function_viper(iters: int) -> int:
+    a = 0
+    for i in range(iters):
+        a = i + 1
+    return a
+
+
 def main():
-    pass
+    my_class = MyClass()
+    iters = 10_000
+
+    for i in range(iters):
+        function(i)
+        function_function(i)
+        function_function_viper(i)
+        function_viper(i)
+        function_viper_function(i)
+        my_class.method(i)
+        my_class.method_viper(i)
+
+    no_function(iters)
+    no_function_viper(iters)
 
 
 if __name__ == "__main__":
